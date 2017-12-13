d744d8bd66b6d6dd1205f24258b7f8e2956e64b2
Woland <pawelgronowskii@gmail.com>
2017-12-13 21:27:12 +0100

    Project, hello world

---
+++ Source/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+
+            int w = Console.WindowWidth;
+            int h = Console.WindowHeight;
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.SetCursorPosition(w / 2 - 10, h / 2);
+            Console.Write("This is my RPG game!");
+
+            Console.ReadLine();
+        }
+    }
+}
