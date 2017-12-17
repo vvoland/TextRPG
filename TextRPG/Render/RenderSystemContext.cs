5e3ebe9fd8d1ce1d86e691706c803c6da7f64804
Woland <pawelgronowskii@gmail.com>
2017-12-17 22:53:06 +0100

    RenderSystemContext: Initialize properties

--- TextRPG/Render/RenderSystemContext.cs
+++ TextRPG/Render/RenderSystemContext.cs
@@ -2,14 +2,27 @@ namespace TextRPG.Render
 {
     public class RenderSystemContext
     {
-        public virtual Vector2 Viewport { get; set; }
+        public virtual Vector2 Clipping { get; set; }
         public virtual Vector2 Translation { get; set; }
+        public bool ClippingEnabled
+        {
+            get
+            {
+                return Clipping.X == 0 && Clipping.Y == 0;
+            }
+        }
+
+        public RenderSystemContext()
+        {
+            Translation = new Vector2(0, 0);
+            Clipping = new Vector2(0, 0);
+        }
 
         public virtual RenderSystemContext Clone()
         {
             RenderSystemContext ctx = new RenderSystemContext()
             {
-                Viewport = this.Viewport,
+                Clipping = this.Clipping,
                 Translation = this.Translation
             };
             
