61c10401bf7cebaf28c0adeb0357f5a0bd6e9f59
Woland <pawelgronowski@protonmail.com>
2018-01-22 21:31:45 +0100

    ViewMainMenu: Initial main menu

--- TextRPG/Game/Views/ViewMainMenu.cs
+++ TextRPG/Game/Views/ViewMainMenu.cs
@@ -1,6 +1,7 @@
 using System;
 using TextRPG.Event;
 using TextRPG.GUI;
+using TextRPG.GUI.Layout;
 using TextRPG.Render;
 using TextRPG.Utils;
 
@@ -8,8 +9,10 @@ namespace TextRPG.Game.Views
 {
     public class ViewMainMenu : View
     {
-        private GUIButton NewGame, Quit;
+        private Label Title, Author, Help;
+        private GUIButton ContinueGame, NewGame, Quit;
         private GUISystem GUI;
+        private LinearLayout ButtonsLayout, MenuLayout;
 
         public ViewMainMenu(GameSystem game, RenderSystem renderer) : base(game, renderer)
         {
@@ -19,11 +22,45 @@ namespace TextRPG.Game.Views
         private void Init()
         {
             GUI = new GUISystem();
-            NewGame = new GUIButton(GetScreenPoint(0.25f, 0.45f), "New Game", () => ActionNewGame());
-            Quit = new GUIButton(GetScreenPoint(0.75f, 0.45f), "Quit", () => Game.Running = false);
 
+            Title = new Label(@" _____         _    ____________ _____
+|_   _|       | |   | ___ \ ___ \  __ \
+  | | _____  _| |_  | |_/ / |_/ / |  \/
+  | |/ _ \ \/ / __| |    /|  __/| | __ 
+  | |  __/>  <| |_  | |\ \| |   | |_\ \
+  \_/\___/_/\_\\__| \_| \_\_|    \____/", GetScreenPoint(0.5f, 0.25f), Vector2f.Center, Color.DarkGray);
+
+            Author = new Label("by Pawel Gronowski", Vector2.Zero, Vector2f.Center, Color.DarkBlue);
+            Help = new Label("TAB: Switch button\nENTER: Activate", Vector2.Zero, Vector2f.Center, Color.DarkGreen);
+
+            MenuLayout = new LinearLayout(LayoutDirection.Vertical, GetScreenPoint(0.5f, 0.5f), Vector2f.Center);
+            MenuLayout.Size = new Vector2((int)Renderer.Size.X, (int)Renderer.Size.Y);
+            MenuLayout.Spacing = 2;
+
+            ButtonsLayout = new LinearLayout(LayoutDirection.Horizontal, GetScreenPoint(0.5f, 0.5f), Vector2f.Center);
+            ButtonsLayout.Spacing = 5;
+            ContinueGame = new GUIButton(Vector2.Zero, "Continue Game", () => ActionContinueGame());
+            NewGame = new GUIButton(Vector2.Zero, "New Game", () => ActionNewGame());
+            Quit = new GUIButton(Vector2.Zero, "Quit", () => Game.Running = false);
+
+            ButtonsLayout.Add(ContinueGame);
+            ButtonsLayout.Add(NewGame);
+            ButtonsLayout.Add(Quit);
+            ButtonsLayout.Size = new Vector2((int)Renderer.Size.X, (int)Renderer.Size.Y / 4);
+
+            MenuLayout.Add(new GUIAdapter(Title));
+            MenuLayout.Add(new GUIAdapter(Author));
+            MenuLayout.Add(new GUIAdapter(Help));
+            MenuLayout.Add(ButtonsLayout);
+
+            GUI.Add(ContinueGame);
             GUI.Add(NewGame);
             GUI.Add(Quit);
+        }
+
+        private void ActionContinueGame()
+        {
+            Logger.Log("TODO");
         }
 
         private void ActionNewGame()
@@ -33,7 +70,7 @@ namespace TextRPG.Game.Views
 
         public override void Update(float dt)
         {
-            GUI.Render(Renderer);
+            Renderer.Render(MenuLayout);
         }
 
         public override bool OnEvent(InputKeyEvent ev)
