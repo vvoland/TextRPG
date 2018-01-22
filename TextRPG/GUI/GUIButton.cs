8c40f12d333a609072689a29c6ce7ccf563deff6
Woland <pawelgronowski@protonmail.com>
2018-01-22 17:10:12 +0100

    GUIButton: Fix different pivots than center

--- TextRPG/GUI/GUIButton.cs
+++ TextRPG/GUI/GUIButton.cs
@@ -1,12 +1,22 @@
 using System;
 using TextRPG.Render;
+using TextRPG.Utils;
 
 namespace TextRPG.GUI
 {
     public class GUIButton : GUIWidget, ISelectable, IActivable, ISizeable
     {
         public override Vector2 Position { get; set; }
-        public override Vector2f Pivot { get; set; }
+        public override Vector2f Pivot
+        {
+            get => Background.Pivot;
+            set
+            {
+                Background.Pivot = value;
+                UpdateLabel();
+            }
+        }
+
         public Color SelectionColor = Color.DarkYellow;
         public Color SelectionFrameColor = Color.DarkGreen;
         public bool Selected
@@ -16,32 +26,26 @@ namespace TextRPG.GUI
         }
         public Vector2 Size 
         { 
-            get
-            {
-                return _Size;
-            }
-            set
-            {
-                _Size = value;
-                if(Background != null)
-                    Background.Size = value;
-            }
+            get => Background.Size;
+            set => Background.Size = value;
         }
 
         private Frame Background;
         private Label Label;
         private Action ActivateCallback;
-        private Vector2 _Size, _Position;
 
         public GUIButton(Vector2 position, string text, Action onActivate)
-            : this(position, null, null, onActivate)
         {
             Label = new Label(text);
             Label.Color = Color.White;
-            Size = Label.CalculateSize().Expand(4, 2);
-            Background = new Frame(Vector2.Zero, Size);
+            Background = new Frame(Vector2.Zero, Vector2.Zero);
             Background.Color = Color.Gray;
             Background.Character = '*';
+
+            Size = Label.CalculateSize().Expand(4, 2);
+            Position = position;
+            Pivot = Vector2f.Center;
+            ActivateCallback = onActivate;
         }
 
         public GUIButton(Vector2 position, Frame background, Label label, Action onActivate)
@@ -78,6 +82,14 @@ namespace TextRPG.GUI
             }
             Selected = selected;
         }
+
+        private void UpdateLabel()
+        {
+            var bounds = BoundsCalculator.Calculate(Vector2.Zero, Pivot, Size);
+            Label.Position = bounds.Center;
+            Label.Pivot = Vector2f.Center;
+        }
+
 
         private void SwapColor(IColorable colorable, ref Color color)
         {
