77c9775a439e9327b511b6ffb0c3c762adf16c66
Woland <pawelgronowski@protonmail.com>
2018-01-27 18:27:59 +0100

    HealthDamageable: Use Stats to calculate max health

--- TextRPG/Game/HealthDamageable.cs
+++ TextRPG/Game/HealthDamageable.cs
@@ -3,7 +3,13 @@ namespace TextRPG.Game
     public class HealthDamageable : IDamageable
     {
         public int Health { get; protected set; }
-        public int MaxHealth { get; set; }
+        public int MaxHealth
+        {
+            get
+            {
+                return Stats.CalculateHealth();
+            }
+        }
 
         public bool IsAlive
         {
@@ -11,6 +17,21 @@ namespace TextRPG.Game
             {
                 return Health > 0;
             }
+        }
+
+        private IStats Stats;
+
+        public HealthDamageable(IStats stats)
+        {
+            Stats = stats;
+            Health = stats.CalculateHealth();
+        }
+
+        public void Heal(int amount)
+        {
+            Health += amount;
+            if(Health > MaxHealth)
+                Health = MaxHealth;
         }
 
         public void ReceiveDamage(ref Damage damage)
