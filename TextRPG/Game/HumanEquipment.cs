7e7865ad928c63550596a4255cf3040b3566de34
Woland <pawelgronowski@protonmail.com>
2018-01-27 15:24:16 +0100

    HumanEquipment

---
+++ TextRPG/Game/HumanEquipment.cs
@@ -0,0 +1,32 @@
+using TextRPG.Game.Mechanics;
+
+namespace TextRPG.Game
+{
+    public class HumanEquipment
+    {
+        public ItemWeapon Weapon
+        {
+            get;
+            protected set;
+        }
+
+        private IInventory Inventory;
+
+        public HumanEquipment(IInventory inventory)
+        {
+        }
+
+        public void Equip(ItemWeapon weapon)
+        {
+            if(Inventory.Has(weapon))
+                Weapon = weapon;
+        }
+
+        public void Unequip(ItemWeapon weapon)
+        {
+            if(Weapon == weapon)
+                Weapon = null;
+        }
+
+    }
+}
