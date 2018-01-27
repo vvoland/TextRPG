57414597a6f22270ad8a736e34834aac821781fc
Woland <pawelgronowski@protonmail.com>
2018-01-27 18:29:30 +0100

    World: Locations

--- TextRPG/Game/World/World.cs
+++ TextRPG/Game/World/World.cs
@@ -5,15 +5,22 @@ namespace TextRPG.Game
     public class World
     {
         private List<Entity> Entities { get; set; }
+        private List<Location> Locations { get; set; }
 
         public World()
         {
             Entities = new List<Entity>();
+            Locations = new List<Location>();
         }
 
         public void Add(Entity entity)
         {
             Entities.Add(entity);
+        }
+
+        public void Add(Location location)
+        {
+            Locations.Add(location);
         }
 
         public void Remove(Entity entity)
