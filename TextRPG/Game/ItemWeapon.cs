5b1f68251dc8bc6df33b86e2e6e4a5f5f17c24de
Woland <pawelgronowski@protonmail.com>
2018-01-27 18:28:12 +0100

    ItemWeapon: Add Strength and Speed

--- TextRPG/Game/ItemWeapon.cs
+++ TextRPG/Game/ItemWeapon.cs
@@ -1,7 +1,39 @@
+using System;
+
 namespace TextRPG.Game
 {
     public class ItemWeapon : Item
     {
+        public int Strength
+        {
+            get;
+            protected set;
+        }
+        public float Speed
+        {
+            get;
+            protected set;
+        }
+
+        public ItemWeapon(string name) : base(name, DefaultPlural(name))
+        {
+            
+        }
+
+        private static string DefaultPlural(string name)
+        {
+            return name + "s";
+        }
+
+        public ItemWeapon(string name, string plural) : base(name, plural)
+        {
+        }
+
+        public ItemWeapon SetStrength(int str)
+        {
+            Strength = str;
+            return this;
+        }
 
         public override bool CanUse(IUsageContext context)
         {
@@ -11,6 +43,12 @@ namespace TextRPG.Game
         public override void Use(IUsageContext context)
         {
             context.Use(this);
+        }
+
+        public ItemWeapon SetSpeed(float speed)
+        {
+            Speed = speed;
+            return this;
         }
     }
 
