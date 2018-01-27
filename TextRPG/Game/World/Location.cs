05ddbb39b27430696cf02207b9fe7317725f1ed1
Woland <pawelgronowski@protonmail.com>
2018-01-27 18:28:22 +0100

    Location: Add links

--- TextRPG/Game/World/Location.cs
+++ TextRPG/Game/World/Location.cs
@@ -11,11 +11,30 @@ namespace TextRPG.Game
             set => Name = value;
         }
 
+        public ICollection<Location> Links
+        {
+            get
+            {
+                return _Links;
+            }
+        }
+
+        private List<Location> _Links;
         private List<Entity> Entities = new List<Entity>();
 
         public Location(string name)
         {
+            _Links = new List<Location>();
             Name = name;
+        }
+
+        public void Link(Location other)
+        {
+            if(!_Links.Contains(other))
+            {
+                _Links.Add(other);
+                other.Link(this);
+            }
         }
 
 
