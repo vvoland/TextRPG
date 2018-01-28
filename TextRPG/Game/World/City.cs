c0e11e3a1f30e45d16d9820123f89963b33d7539
Woland <pawelgronowski@protonmail.com>
2018-01-28 15:56:59 +0100

    City: implement DisplayName

--- TextRPG/Game/World/City.cs
+++ TextRPG/Game/World/City.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
+
 namespace TextRPG.Game
 {
     public class City : Location
     {
-
+        public override string DisplayName
+        {
+            get
+            {
+                return "City " + Name;
+            }
+        }
 
         public City(string name) : base(name)
         {
         }
-
 
     }
 }
